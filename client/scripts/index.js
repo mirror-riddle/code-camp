d18597e90e06043f65ab198b2f21bc766e1aa74b
hejin <hejinpy@163.com>
2020-04-17 09:46:06 +0800

    add slide view animation

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,11 +1,33 @@
-const modalView = document.querySelector('.modal-view');
+(() => {
+  const modalView = document.querySelector('.modal-view');
 
-const hideModal = document.querySelector('.hide-modal');
-hideModal.addEventListener('click', (event) => {
-  modalView.classList.remove(['visible']);
-}, false);
+  const hideModal = document.querySelector('.hide-modal');
+  hideModal.addEventListener(
+    'click',
+    (event) => {
+      modalView.classList.remove(['visible']);
+    },
+    false
+  );
 
-const showModal = document.querySelector('.show-modal');
-showModal.addEventListener('click', (event) => {
-  modalView.classList.add(['visible']);
-}, false);
+  const showModal = document.querySelector('.show-modal');
+  showModal.addEventListener(
+    'click',
+    (event) => {
+      modalView.classList.add(['visible']);
+    },
+    false
+  );
+
+  const viewContainer = document.querySelector('.view-container');
+  const viewList = document.querySelector('.view-list');
+  const viewDetail = document.querySelector('.view-detail');
+
+  const onViewChange = (event) => {
+    viewContainer.classList.toggle('view-change');
+  };
+
+  [viewList, viewDetail].map((view) => {
+    view.addEventListener('click', onViewChange, false);
+  });
+})();
