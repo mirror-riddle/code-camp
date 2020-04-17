9bec510ba9527af3305c773eda0c8844a1690b5f
mirror-riddle <hejinpy@163.com>
2020-04-18 03:54:04 +0800

    async module script

---
+++ client/scripts/listeners.js
@@ -0,0 +1,42 @@
+export const addEventListeners = () => {
+  const modalView = document.querySelector('.modal-view');
+
+  const hideModal = document.querySelector('.hide-modal');
+  hideModal.addEventListener(
+    'click',
+    (event) => {
+      modalView.classList.remove(['visible']);
+    },
+    false
+  );
+
+  const showModal = document.querySelector('.show-modal');
+  showModal.addEventListener(
+    'click',
+    (event) => {
+      modalView.classList.add(['visible']);
+    },
+    false
+  );
+
+  const viewContainer = document.querySelector('.view-container');
+  const viewList = document.querySelector('.view-list');
+  const viewDetail = document.querySelector('.view-detail');
+
+  const onViewChange = (event) => {
+    viewContainer.classList.toggle('view-change');
+  };
+
+  [viewList, viewDetail].map((view) => {
+    view.addEventListener('click', onViewChange, false);
+  });
+
+  const clickCall = document.querySelector('.click-call');
+  clickCall.addEventListener(
+    'click',
+    (event) => {
+      event.stopPropagation();
+    },
+    true
+  );
+};
