980e014fc0de16995ba8c352e0faef4b4f85b6ba
hejin <hejinpy@163.com>
2020-04-20 17:07:37 +0800

    two servers

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -1,3 +1,11 @@
 import { addEventListeners } from './listeners.js';
 
 addEventListeners();
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `http://localhost:4001/cors-cookies?cookies=${document.cookie}`, true);
+xhr.withCredentials = true;
+xhr.onload = () => {
+  console.log(xhr.response);
+};
+xhr.send();
--- server/index.js
+++ server/index.js
@@ -25,3 +25,25 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`server runing at http://localhost:${port}`);
 });
+
+const server1 = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Set-Cookie', [
+      `name=hejin; Max-Age=60; Path=/; Domain=127.0.0.1; HttpOnly; SameSite=Lax`,
+      'age=28'
+    ]);
+  }
+  if (req.url.startsWith('/cors-cookies')) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    console.log(req.method);
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    console.log(url);
+    res.write('recived');
+    res.end();
+  }
+});
+
+server1.listen(4001, () => {
+  console.log(`server runing at http://localhost:4001`);
+});
