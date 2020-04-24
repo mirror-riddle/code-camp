9f95a6fa525d935a52f3172fcff40efc76716b31
hejin <hejinpy@163.com>
2020-04-24 16:02:28 +0800

    update abort-signal

--- client/scripts/abort-signal.js
+++ client/scripts/abort-signal.js
@@ -18,8 +18,34 @@ const abortableExecutorDecorator = (executor) => (resolve, reject) => {
   executor(resolve, reject);
 };
 
-export const useAbortablePromise = (executor) => {
+const useAbortablePromise = (executor) => {
   const abortableExecutor = abortableExecutorDecorator(executor);
   const promise = new Promise(abortableExecutor);
   return { promise, controller };
+};
+
+export const testAbortSignal = () => {
+  console.time('promise-timelog');
+
+  const { promise, controller } = useAbortablePromise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('time up');
+    }, 2000);
+  });
+
+  // console.timeLog('promise-timelog', 'aborted before promise is created...');
+  // controller.abort();
+
+  promise
+    .then((value) => {
+      console.timeLog('promise-timelog', value);
+    })
+    .catch((error) => {
+      console.error('error catched:', error);
+    });
+
+  setTimeout(() => {
+    console.timeLog('promise-timelog', 'aborted when promise is working...');
+    controller.abort();
+  }, 3000);
 };
